02b04ad1f50127e8523bb4b8b5ed6b523d460d6c
Javier Maria <javier.maria.salas@gmail.com>
2023-01-30 20:09:27 -0500

    Login Form v1 created

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,50 @@
+import { useState, useRef } from 'react';
+import '../css/Login.css';
+
 const Login = () => {
+    const [ value, setValue ] = useState({ user: '', password: '', buttonPushed: false });
+    const inputName = useRef();
+    const inputPassword = useRef();
+
+
+    const handleChange = e => {
+        setValue({
+            ...value,
+            [e.target.name]: e.target.value
+        });
+
+        console.log(value);
+    }
+
+    const startSesion = () => {
+        setValue({
+            ...value,
+            ['buttonPushed']: true
+        });
+        console.log(value.buttonPushed);
+    }
     return(
-        <h1>Este es el componente de login</h1>
+        <div>
+            <h1>Login</h1>
+
+            <div>
+                <h2>Bienvenido</h2>
+
+                <div>
+                    <div>
+                        <label>Usuario</label>
+                        <input name='user' value={value.user} onChange={handleChange} ref={inputName}/>
+                    </div>
+                    <div>
+                        <label>Contraseña</label>
+                        <input name='password' value={value.password} onChange={handleChange} ref={inputPassword}/>
+                        {console.log(`Longitud de la cadena: ${value.password.length}`)}
+                        {value.buttonPushed && value.password.length === 0 ? <p>Debes ingresar una contraseña</p> : <span></span>}
+                    </div>
+                    <input type='submit' value='Iniciar Sesión' onClick={startSesion}/>
+                </div>
+            </div>
+        </div>
     )
 }
 
